1abee72a2a02035f481accbf932097b35f740b6e
fibonacci <zepeng816125@163.com>
2020-07-03 20:48:05 +0800

    添加必要注释

--- 1104 Sum of Number Segments.cpp
+++ 1104 Sum of Number Segments.cpp
@@ -10,7 +10,7 @@ int main()
     long long sum = 0;
 	for(int i = 0; i < N; i++){
 		scanf("%lf", &number);
-		sum += (N-i)*(i+1) * (long long)(number * 1000);
+		sum += (N-i)*(i+1) * (long long)(number * 1000);  // 解决double类型数值在大量累乘累加后造成的精度误差；若inputs的小数点位数大于3，那么当前代码不再保证能得到正确答案
 	} 
 	printf("%.2lf", sum/1000.0);
 	return 0;
